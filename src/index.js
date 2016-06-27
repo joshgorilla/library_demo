17b0a3b2f5fbfc4e45a138178a127025d5b5cf7b
Josh Nykamp <josh.nykamp@gorillalogic.com>
2016-06-27 09:40:11 -0600

    Going through demo

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var starWarsNames = require('./star-wars-names.json');
+var uniqueRandomArray = require('unique-random-array');
+
+module.exports = {
+    all: starWarsNames,
+    random: uniqueRandomArray(starWarsNames)
+};
